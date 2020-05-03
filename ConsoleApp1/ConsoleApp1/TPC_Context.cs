00897a5aaeff0052f56a951feb504cd583ebdbe5
Jakub Sałamun <zellanderplx@gmail.com>
2020-05-03 16:14:01 +0200

    das

---
+++ ConsoleApp1/ConsoleApp1/TPC_Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TPC_Context : DbContext
+    {
+        public DbSet<Computer> Komputery { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Pc>().Map(x =>
+            {
+                x.MapInheritedProperties();
+                x.ToTable("Pecety");
+
+            });
+
+            modelBuilder.Entity<Laptop>().Map(x =>
+            {
+                x.MapInheritedProperties();
+                x.ToTable("Laptopy");
+            });
+        }
+    }
+}
